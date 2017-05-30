5a3c36a974381cdda9d0531f3db5710867f69ffb
Sean Cross <sean@xobs.io>
2017-05-30 17:52:00 +0800

    before-object-refactor
    Signed-off-by: Sean Cross <sean@xobs.io>

--- src/main.rs
+++ src/main.rs
@@ -19,65 +19,53 @@
 //! Will update in the future to support all HIDs. 
 
 extern crate hidapi;
-
 use hidapi::HidApi;
+pub mod bootloader;
 
-struct BlPkt {
-    cmd: u8,
-    seq: u8,
-    data: [u8; 6],
-}
+const EP_NUM: u8 = 2;
 
 fn main() {
     let api = HidApi::new().expect("Failed to create API instance");
 
+    /*
     println!("List of devices:");
     for dev in api.devices() {
         println!("    Device: {:?}", dev);
     }
+    */
     let joystick = api.open(0x1bcf, 0x05ce).expect("Failed to open device");
-    let mut pkt_num = 0;
-    let offset = 0u32;
+    println!("Product: {}", joystick.get_product_string().expect("Unable to get product string"));
 
-    loop {
-        let mut in_buf = [0u8; 9];
-        let mut out_buf = [0u8; 9];
+    let mut bl_cmd = bootloader::make_info_cmd(EP_NUM, 0);
+    let mut in_buf = [0u8; 8];
 
-        out_buf[0] = 2;
-        out_buf[1] = pkt_num;
-        out_buf[2] = 5;
+    // Dummy read to prime the HID buffer
+    joystick.read_timeout(&mut in_buf[..], 1).expect("BL_INFO returned no data");
 
-        out_buf[3] = 0;
-        out_buf[4] = 1;
-
-        out_buf[5] = (offset & 0xff) as u8;
-        out_buf[6] = ((offset >> 8) & 0xff) as u8;
-        out_buf[7] = ((offset >> 16) & 0xff) as u8;
-        out_buf[8] = ((offset >> 24) & 0xff) as u8;
-
-        match joystick.write(&mut out_buf[..]) {
-            Ok(_) => (),//println!("Wrote {} bytes", write_res),
-            //Err(msg) => panic!("An error occurred while writing: {}", msg),
-            Err(msg) => {println!("An error occurred while writing: {}", msg); continue},
+    joystick.write(&mut bl_cmd[..]).expect("Unable to write BL_INFO command");
+    let result = joystick.read_timeout(&mut in_buf[..], 100).expect("BL_INFO returned no data");
+    if result == 0 {
+        joystick.write(&mut bl_cmd[..]).expect("Unable to write BL_INFO command");
+        let result = joystick.read_timeout(&mut in_buf[..], 100).expect("BL_INFO returned no data");
+        if result == 0 {
+            panic!("Other end returned no data");
         }
-        
-        //std::thread::sleep(std::time::Duration::from_millis(20));
-        match joystick.read_timeout(&mut in_buf[..], 1000) {
-            Ok(res) if res == 0 => {println!("No data read"); continue; },
-            Err(_) => {println!("Unknown error occurred when reading"); continue; },
-            Ok(res) => {
-                println!("Done with read, formatting {} bytes", res);
-                let mut data_string = String::new();
+        println!("Note: Got it on the second try.");
+    }
+    println!("Raw bootlaoder data: {:?}", in_buf);
+    println!("Decoded bootloader data: {:?}", bootloader::decode_bootloader_info(in_buf));
 
-                for u in &in_buf[..res] {
-                    data_string.push_str(&(u.to_string() + "\t"));
-                }
-
-                println!("{}", data_string);
-        
-                pkt_num = pkt_num.wrapping_add(1);
-                //offset += 256;
-            },
-        }
+    let mut echo_cmd = bootloader::make_echo_cmd(EP_NUM, 9);
+    joystick.write(&mut echo_cmd[..]).expect("Unable to write echo command");
+    let result = joystick.read(&mut in_buf[..]).expect("Unable to read echo command back");
+    assert!(result != 0);
+    println!("Orig: {:?},  Readback: {:?}", echo_cmd, in_buf);
+    // Sanity: Check that the data coming back is the data we sent.
+    assert!(in_buf[1] == 0); // Check that the return code is "no error"
+    assert!(in_buf[0] & 0xf0 == echo_cmd[1] & 0xf0); // Check that the sequence number is the same
+    assert!(in_buf[0] & 0x0f == 15); // Check that the return type is "Result"
+    for i in 2 .. 8 {
+        println!("Testing in_buf[{}]", i);
+        assert!(in_buf[i] == echo_cmd[i + 1]);
     }
 }
