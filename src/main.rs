b907afaaf193fbf6652bf5afaacb0975d4167014
Sean Cross <sean@xobs.io>
2017-06-02 23:15:18 +0800

    update vid/pid
    This uses the assigned Palawan IDs now.
    
    Signed-off-by: Sean Cross <sean@xobs.io>

--- src/main.rs
+++ src/main.rs
@@ -1,21 +1,17 @@
-/****************************************************************************
-    Copyright (c) 2015 Osspial All Rights Reserved.
-    This file is part of hidapi-rs, based on hidapi_rust by Roland Ruckerbauer.
-    hidapi-rs is free software: you can redistribute it and/or modify
-    it under the terms of the GNU General Public License as published by
-    the Free Software Foundation, either version 3 of the License, or
-    (at your option) any later version.
-    hidapi-rs is distributed in the hope that it will be useful,
-    but WITHOUT ANY WARRANTY; without even the implied warranty of
-    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-    GNU General Public License for more details.
-    You should have received a copy of the GNU General Public License
-    along with hidapi-rs.  If not, see <http://www.gnu.org/licenses/>.
-****************************************************************************/
-
-//! Opens a Thrustmaster T-Flight HOTAS X HID and reads data from it. This 
-//! example will not work unless such an HID is plugged in to your system. 
-//! Will update in the future to support all HIDs. 
+/// **************************************************************************
+/// Copyright (c) 2015 Osspial All Rights Reserved.
+/// This file is part of hidapi-rs, based on hidapi_rust by Roland Ruckerbauer.
+/// hidapi-rs is free software: you can redistribute it and/or modify
+/// it under the terms of the GNU General Public License as published by
+/// the Free Software Foundation, either version 3 of the License, or
+/// (at your option) any later version.
+/// hidapi-rs is distributed in the hope that it will be useful,
+/// but WITHOUT ANY WARRANTY; without even the implied warranty of
+/// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+/// GNU General Public License for more details.
+/// You should have received a copy of the GNU General Public License
+/// along with hidapi-rs.  If not, see <http://www.gnu.org/licenses/>.
+/// *************************************************************************
 
 extern crate hidapi;
 extern crate clap;
@@ -49,7 +45,7 @@ fn main() {
         println!("    Device: {:?}", dev);
     }
 
-    let joystick = api.open(0x1bcf, 0x05ce).expect("Failed to open device");
+    let joystick = api.open(0x1209, 0x9317).expect("Failed to open device");
     let mut bl = bootloader::Bootloader::new(joystick);
 
     bl.print_info();
